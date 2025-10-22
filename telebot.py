d9a5599f8c73a56d83ff5e1e6004df20710bd32f
kicapman1x <ahhannan995@gmail.com>
2025-10-22 17:38:28 +0800

    read path for chromadb

--- telebot.py
+++ telebot.py
@@ -1,20 +1,26 @@
 import os
 import chromadb
+import chromadb.utils.embedding_functions as embedding_functions
 import logging
 import json
+import numpy as np
 from openai import OpenAI
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from dotenv import load_dotenv
+# from asyncio import to_thread
 
 logger = logging.getLogger(__name__)
 openai_model = None
 client = None 
 bot_token = None
+chroma_client = None
+collection = None
+openai_ef = None 
 
 def bootstrap():
     #ENV VAR
-    global client, openai_model, bot_token
+    global client, openai_model, bot_token, chroma_client, collection, openai_ef
     load_dotenv()
 
     bot_token = os.environ.get("bot_token")
@@ -23,11 +29,17 @@ def bootstrap():
     openai_model=os.environ.get("openai_model")
     embed_model=os.environ.get("embed_model")
     chromadir = os.environ.get("chroma_directory", ".")
-    fill_db= bool(os.environ.get("fill_db", "False"))
-
+    fill_db = os.environ.get("fill_db", "False").lower() == "true"
+    openai_api_key=os.environ.get("openai_token")
     #OpenAI client
     client = OpenAI(
-        api_key=os.environ.get("openai_token")
+        api_key=openai_api_key
+    )
+
+    #OpenAI ef
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key=openai_api_key,
+        model_name=embed_model
     )
 
     #logger
@@ -40,17 +52,25 @@ def bootstrap():
     )
 
     #ChromaDB 
-    if (fill_db):
-        writeChromaDB(chromadir,embed_model)
-    logger.info('*********** Bootstrap completed ***********')
-
-def writeChromaDB(chromadir,embed_model):
     chroma_client = chromadb.PersistentClient(path=chromadir)
     collection = chroma_client.get_or_create_collection(name="utm_jb_campus_locations")
+    #update db
+    if (fill_db):
+        logger.info('Collection has documents')
+        writeChromaDB()
+        logger.info('Database has been populated with initial data.')
+    else:
+        logger.info('Database already populated or fill_db is False. Skipping population.')
+    logger.info('*********** Bootstrap completed ***********')
+
+def writeChromaDB():
     with open("chroma_input.json", "r", encoding="utf-8") as f:
         data = json.load(f)
+    logger.info(f'Provisioning ids')
     ids = [location['id'] for location in data]
+    logger.info(f'Provisioning docs')
     documents = [location['name'] for location in data]
+    logger.info(f'Provisioning metadatas')
     metadatas = [{
         "id": location['id'],
         "name": location['name'],
@@ -58,22 +78,20 @@ def writeChromaDB(chromadir,embed_model):
         "latitude": location['latitude'],
         "longitude": location['longitude']
     } for location in data]
-    embeddings = [get_embeddings(location['name'],embed_model) for location in data]
+    logger.info('Provisioning embeddings')
+    logger.info(documents)
+    embedding=openai_ef(documents)
+    logger.info(embedding)
+    # embeddings = [openai_ef(location['name'])[0].tolist() for location in data]
+    # logger.info(f"Generated embeddings for data, example: {embeddings[:1]}")
+    logger.info(f'Updating collections')
     collection.upsert(
         ids=ids,
         documents=documents,
         metadatas=metadatas,
-        embeddings=embeddings
+        embeddings=embedding
     )
     logger.info('*********** Data written to ChromaDB ***********')
-
-def get_embeddings(text,embed_model):
-    # Helper for chromadb writer
-    response = client.embeddings.create(
-        model=embed_model, 
-        input=text
-    )
-    return response.data[0].embedding
 
 async def start(update, context):
     await update.message.reply_text("Hello! I'm your friendly UTM Campus Assistant. How may I assist you today?")
@@ -99,9 +117,28 @@ async def handleEscape(message):
 
 async def handleGPT(message):
     try:
+        chromadb_data_result=None 
+        result_docs=''
+        result_metas=''
+        #preferrably a validator, for testing, we will just leave it as if True
+        if (True):
+            chromadb_data_result=await query_chroma_db(message)
+        if chromadb_data_result: 
+            logger.info("documents returned from ChromaDB")
+            result_docs=str(chromadb_data_result['documents'])
+            result_metas=str(chromadb_data_result['metadatas'])
+        else:
+            logger.error("No valid documents returned from ChromaDB")
         response = client.responses.create(
             model=openai_model,
-            instructions="You are a friendly UTM campus assistance that helps students navigate around campus and around campus rules and regulations.",
+            instructions="""
+            You are a friendly UTM campus assistance that helps students navigate around campus and around campus rules and regulations. Upstream from asking you, I have ran a chromadb query and I will provide the relevant data below, you should try your best as possible to use the data given, if suitable. 
+            
+            The data: 
+
+            """+result_docs+"""
+            """+result_metas+"""
+            """,
             input=message,
         )
         gpt_reply=response.output_text
@@ -110,6 +147,17 @@ async def handleGPT(message):
     except Exception as e: 
         logger.error(f"Error with OpenAI API: {e}")
         return "Apologies, I could not process that request. Please contact the administrator."
+
+async def query_chroma_db(message):
+    query_embeddings = openai_ef([message])
+    # query_embeddings = query_embeddings[0].tolist()
+    logger.info(f"Received message and embedded to: {query_embeddings}")
+    results = collection.query(
+        query_embeddings=query_embeddings,
+        n_results=3
+    )
+    logger.info(f"Returning: {results}")
+    return results
 
 def main():
     logger.info('***********Starting UTM Campus Assistance Bot***********')
